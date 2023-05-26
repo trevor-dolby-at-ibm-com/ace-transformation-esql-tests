02b44238c9e21a8f13a1d9291eaa414425235555
Trevor Dolby <trevor.dolby@ibm.com>
2023-05-26 13:20:52 -0500

    Add Windows build script

---
+++ build-and-ut.cmd
@@ -0,0 +1,21 @@
+REM
+REM This script automates the building and testing of the application.
+REM
+REM Copyright (c) 2023 Open Technologies for Integration
+REM Licensed under the MIT license (see LICENSE for details)
+REM
+
+
+REM Create the work directory
+rmdir /s /q %TEMP%\ace-transformation-esql-tests-work-dir
+call mqsicreateworkdir %TEMP%\ace-transformation-esql-tests-work-dir
+
+REM Build everything; we can do this in this case because we want to include the unit
+REM tests, but production builds should specify the projects.
+ibmint deploy --input-path . --output-work-directory %TEMP%\ace-transformation-esql-tests-work-dir
+
+REM ibmint optimize server new for v12.0.4 - speed up test runs
+ibmint optimize server --work-directory %TEMP%\ace-transformation-esql-tests-work-dir --disable NodeJS
+
+REM Run the server to run the unit tests
+IntegrationServer -w %TEMP%\ace-transformation-esql-tests-work-dir --test-project Transformation_ESQL_Test --test-junit-options "--reports-dir=junit-reports"
